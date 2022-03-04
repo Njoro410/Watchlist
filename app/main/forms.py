b4305de8255854c3ccb2913c825c4a27395f7aa1
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-04 18:56:56 +0300

    added more program files

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Required
 
 class ReviewForm(FlaskForm):
 
     title = StringField('Review title',validators=[DataRequired()])
     review = TextAreaField('Movie review', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
